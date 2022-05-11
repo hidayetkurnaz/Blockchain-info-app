b3e37c7e71e9be54cadab843e726ed95df54a070
hidayetkurnaz <hidayetkurnaz@gmail.com>
2022-05-11 21:02:36 +0300

    nothing

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Blockchain-info-app
 
+
--- src/main.rs
+++ src/main.rs
@@ -72,10 +72,10 @@ fn blockchain_info_app(address: &str) {
          balance += &subtotal_vout - &subtotal_vin;
 
          println!("-------------------------------------------------------");
-            println!("TX ID :           {}", &blockchain_transaction.txid);
-            println!("SATOSHIS IN :     {}", &subtotal_vout);
+            println!("TX ID        :    {}", &blockchain_transaction.txid);
+            println!("SATOSHIS IN  :    {}", &subtotal_vout);
             println!("SATOSHIS OUT :    {}", &subtotal_vin);
-            println!("BALANCE :         {}", &balance);
+            println!("BALANCE      :    {}", &balance);
             println!("-------------------------------------------------------");
 
       };
